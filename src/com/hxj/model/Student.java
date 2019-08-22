2f2628da3d9b7c5be545cb5279b50a858a5886e5
hxj <hxj1213@126.com>
2019-08-22 11:37:24 +0800

    init commit

---
+++ src/com/hxj/model/Student.java
@@ -0,0 +1,43 @@
+package com.hxj.model;
+
+/**
+ * @ author 黑潇君
+ * 东软睿道西安TTC
+ */
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
